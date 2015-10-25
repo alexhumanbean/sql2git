0dedbaf2ec2c6a9c194d7b84f71e8ef78983c3c6
AlexHumanBean <niihim-st@yandex.ru>
2015-10-25 16:19:08 +0300

    reading log

---
+++ src/com/sql2git/Config.java
@@ -0,0 +1,50 @@
+package com.sql2git;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import javax.swing.*;
+import java.io.*;
+
+/**
+ * Created by Alex on 25.10.2015.
+ */
+public class Config {
+    public static String CONF_CONFIG_FILE = "config.json";
+    public static String CONF_REPOSITORY_PATH;
+    public static String CONF_WORKING_DIR;
+
+    public static void LoadConfig() {
+        String json = "";
+        try {
+            File file = new File(CONF_CONFIG_FILE);
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(file), "UTF-8"
+                    )
+            );
+            String line;
+            while ((line = br.readLine()) != null) {
+                json += line;
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Не найден конфигурационный файл:\n" + CONF_CONFIG_FILE);
+        } catch (UnsupportedEncodingException e) {
+            JOptionPane.showMessageDialog(null, "Неподдерживаемая кодировка файла:\n" + CONF_CONFIG_FILE);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Не удается прочитать файл:\n" + CONF_CONFIG_FILE);
+        }
+
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObj;
+        try {
+            jsonObj = (JSONObject) parser.parse(json);
+            CONF_REPOSITORY_PATH = (String) jsonObj.get("CONF_REPOSITORY_PATH");
+            CONF_WORKING_DIR = (String) jsonObj.get("CONF_WORKING_DIR");
+        } catch (ParseException e) {
+            JOptionPane.showMessageDialog(null, "Неправильный формат конфигурационного файла:\n" + e.toString());
+        }
+    }
+}
--- src/com/sql2git/Main.java
+++ src/com/sql2git/Main.java
@@ -1,8 +1,51 @@
 package com.sql2git;
 
-public class Main {
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.filter.PathFilter;
 
-    public static void main(String[] args) {
-	// write your code here
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        Config.LoadConfig();
+        File gitWorkDir = new File(Config.CONF_WORKING_DIR);
+        Git git = Git.open(gitWorkDir);
+        Repository repo = git.getRepository();
+
+        ObjectId lastCommitId = repo.resolve(Constants.HEAD);
+
+        RevWalk revWalk = new RevWalk(repo);
+        RevCommit commit = revWalk.parseCommit(lastCommitId);
+
+        RevTree tree = commit.getTree();
+
+        TreeWalk treeWalk = new TreeWalk(repo);
+        treeWalk.addTree(tree);
+        treeWalk.setRecursive(true);
+        treeWalk.setFilter(PathFilter.create("README.md"));
+        if (!treeWalk.next())
+        {
+            System.out.println("Nothing found!");
+            return;
+        }
+
+        ObjectId objectId = treeWalk.getObjectId(0);
+        ObjectLoader loader = repo.open(objectId);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        loader.copyTo(out);
+        System.out.println("README.md:\n" + out.toString());
     }
 }
